17918b8ea36360b54d247329935d46a98ee7b1f5
Hoang Lan <lanvth97@gmail.com>
2022-03-17 00:47:41 +0700

    add project

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = (BASE_DIR /"staticfile")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(PROJECT_DIR, 'static')]
 
 # media files config
