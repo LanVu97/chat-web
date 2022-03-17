0a20378dc0b5475a078004d350b0ada0c4fb0347
Hoang Lan <lanvth97@gmail.com>
2022-03-17 09:47:11 +0700

    add django-heroku

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -181,3 +181,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
