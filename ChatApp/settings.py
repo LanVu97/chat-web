8ee9050cf64dcf46bafe2baad9f5db983e186ffc
Hoang Lan <lanvth97@gmail.com>
2022-03-17 16:39:03 +0700

    change whitenote position

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -183,12 +183,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(PROJECT_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(PROJECT_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    [os.path.join(PROJECT_DIR, 'static')],
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     [os.path.join(PROJECT_DIR, 'static')],
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # media files config
 
 MEDIA_URL = '/media/'
