b43085e4699b867eba549857f0d02072ef63e78f
Hoang Lan <lanvth97@gmail.com>
2022-03-17 10:43:16 +0700

    change database

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -124,10 +124,20 @@ WSGI_APPLICATION = 'ChatApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'f5fp4fv8g04j7',
+        'USER': 'pjvwofvjmpvunz',
+        'PASSWORD': '99fa1dbf68af53556b2fbbef491c2e552100dd90214efab5e295820b44602aa8',
+        'HOST': 'ec2-34-203-255-149.compute-1.amazonaws.com',
+        'PORT':  '5432'
     }
 }
 
