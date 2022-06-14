ef422392f7432ceb09e339bdc1399c45cf4db099
Hoang Lan <lanvth97@gmail.com>
2022-06-15 00:24:49 +0700

    delete whitenoise in installs app

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'social_django',
     'chat',
     # deploy
-     'whitenoise.runserver_nostatic',
+    #  'whitenoise.runserver_nostatic',
 ]
 
 AUTH_USER_MODEL = 'account.AccountUser'
@@ -182,14 +182,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join(PROJECT_DIR, 'static/')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(PROJECT_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# media files config
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
