f98c1a7c4a72e8630c5fbb322bea9aaa8f0551b9
Hoang Lan <lanvth97@gmail.com>
2022-06-15 07:22:34 +0700

    update url

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -27,6 +27,4 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('chat/', include('chat.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+
